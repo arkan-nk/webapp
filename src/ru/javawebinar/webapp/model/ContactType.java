4573dc845aa228a029afc2c075b58d89d435c92c
Arkan_nk <arkan.nk@gmail.com>
2016-04-04 17:49:01 +0300

    home work 2 ContactType have captions

--- src/ru/javawebinar/webapp/model/ContactType.java
+++ src/ru/javawebinar/webapp/model/ContactType.java
@@ -4,8 +4,9 @@ package ru.javawebinar.webapp.model;
  * Created by arkan on 04.04.2016.
  */
 public enum ContactType {
-    LOCATION (0,"Проживание"), CELLPHONE(1, "Тел."), EMAILHOME(2, ""), SKYPE(3, ""),
-    HABRPROFILE(4, ""), STACKOVFPROFILE(5, ""), GITHUBACC(6, ""), LINKEDLNPROFILE(7,"");
+    LOCATION (0,"Проживание"), CELLPHONE(1, "Тел."), EMAILHOME(2, "e-mail"), SKYPE(3, "skype"),
+    HABRPROFILE(4, "Habrhabr"), STACKOVFPROFILE(5, "stackoverflow"), GITHUBACC(6, "github"),
+    LINKEDLNPROFILE(7,"LinkedIn");
 
     ContactType(Integer idd, String caption1){
         id=idd;
