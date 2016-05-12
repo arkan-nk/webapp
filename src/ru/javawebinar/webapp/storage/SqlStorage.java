8d2feeabbbbe540ece41691f7a8dd93ff10bfaf5
Arkan_nk <arkan.nk@gmail.com>
2016-05-12 18:18:38 +0300

    HW10

--- src/ru/javawebinar/webapp/storage/SqlStorage.java
+++ src/ru/javawebinar/webapp/storage/SqlStorage.java
@@ -8,10 +8,7 @@ import ru.javawebinar.webapp.sql.SqlHelper;
 import ru.javawebinar.webapp.util.HtmlUtil;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * GKislin
@@ -75,12 +72,102 @@ public class SqlStorage implements Storage {
                     throw new ResumeNotFoundException(r.getUuid());
                 }
             }
-            deleteContacts(conn, r);
-            insertContact(conn, r);
+            refreshContact(r);
+            //deleteContacts(conn, r);
+            //insertContact(conn, r);
             return null;
         });
     }
 
+    private void refreshContact(Resume resume) throws SQLException {
+        Resume clone = this.get(resume.getUuid());
+        if (resume.getContacts().isEmpty() && clone.getContacts().isEmpty()) return;
+        if (resume.getContacts().isEmpty() && !clone.getContacts().isEmpty()) {
+            deleteMapContacts(clone.getUuid(), clone.getContacts());
+            return;
+        }
+        refreshContact(resume, clone);
+    }
+
+    private void refreshContact(Resume resume, Resume clone) throws SQLException{
+        if (resume.getContacts().isEmpty() && clone.getContacts().isEmpty()) return;
+        if (resume.getContacts().isEmpty() && !clone.getContacts().isEmpty()) {
+            deleteMapContacts(resume.getUuid(), clone.getContacts());
+            return;
+        }
+        if (!resume.getContacts().isEmpty() && clone.getContacts().isEmpty()) {
+            addMapContacts(resume.getUuid(), resume.getContacts());
+            return;
+        }
+        Map<ContactType, String> contactsToAdd = new EnumMap<>(ContactType.class);
+        Map<ContactType, String> contactsToRemove = new EnumMap<>(ContactType.class);
+        Map<ContactType, String> contactsToUpdate = new EnumMap<>(ContactType.class);
+        for (Map.Entry<ContactType,String> entryResume : resume.getContacts().entrySet()){
+            ContactType keyResume = entryResume.getKey();
+            if (clone.getContacts().containsKey(keyResume) &&
+                    !clone.getContacts().get(keyResume).equals(entryResume.getValue())) {
+                contactsToUpdate.put(keyResume, entryResume.getValue());
+                continue;
+            }
+            if (!clone.getContacts().containsKey(keyResume)) {
+                contactsToAdd.put(keyResume, entryResume.getValue());
+            }
+        }
+        for (Map.Entry<ContactType,String> entryClone : clone.getContacts().entrySet()){
+            ContactType keyClone = entryClone.getKey();
+            if (!resume.getContacts().containsKey(keyClone)) {
+                contactsToRemove.put(keyClone, entryClone.getValue());
+            }
+        }
+        if (!contactsToAdd.isEmpty()) addMapContacts(resume.getUuid(), contactsToAdd);
+        if (!contactsToRemove.isEmpty()) deleteMapContacts(resume.getUuid(),contactsToRemove);
+        if (!contactsToUpdate.isEmpty()) updateMapContacts(resume.getUuid(),contactsToUpdate);
+    }
+
+    private void updateMapContacts(String uuid, Map<ContactType, String> contactsToUpdate) throws SQLException {
+        sqlHelper.transactionExecute(conn -> {
+            try (PreparedStatement st = conn.prepareStatement(
+                    "update contact set value=? where RESUME_UUID=? and TYPE=?")) {
+                for (Map.Entry<ContactType, String> e : contactsToUpdate.entrySet()) {
+                    st.setString(1, e.getValue());
+                    st.setString(2, uuid);
+                    st.setString(3, e.getKey().name());
+                    st.addBatch();
+                }
+                st.executeBatch();
+            }
+            return null;
+        });
+    }
+
+    private void deleteMapContacts(String uuid, Map<ContactType, String> contactsToRemove) throws SQLException {
+        sqlHelper.transactionExecute(conn -> {
+            try (PreparedStatement st = conn.prepareStatement("DELETE FROM contact WHERE resume_uuid=? AND type=?")) {
+                for (Map.Entry<ContactType, String> e : contactsToRemove.entrySet()) {
+                    st.setString(1, uuid);
+                    st.setString(2, e.getKey().name());
+                    st.addBatch();
+                }
+                st.executeBatch();
+            }
+            return null;
+        });
+    }
+
+    private void addMapContacts(String uuid, Map<ContactType, String> contactsToAdd) throws SQLException {
+        sqlHelper.transactionExecute(conn -> {
+            try (PreparedStatement st = conn.prepareStatement("INSERT INTO contact (resume_uuid, type, value) VALUES (?,?,?)")) {
+                for (Map.Entry<ContactType, String> e : contactsToAdd.entrySet()) {
+                    st.setString(1, uuid);
+                    st.setString(2, e.getKey().name());
+                    st.setString(3, e.getValue());
+                    st.addBatch();
+                }
+                st.executeBatch();
+            }
+            return null;
+        });
+    }
     @Override
     public void delete(String uuid) {
         sqlHelper.execute("DELETE FROM resume WHERE uuid=?", ps -> {
@@ -143,11 +230,14 @@ public class SqlStorage implements Storage {
         }
     }
 
-    private void deleteContacts(Connection conn, Resume r) throws SQLException {
-        try (PreparedStatement st = conn.prepareStatement("DELETE FROM contact WHERE resume_uuid = ?")) {
-            st.setString(1, r.getUuid());
-            st.execute();
-        }
+    private void deleteContacts(Resume r) throws SQLException {
+        sqlHelper.transactionExecute(conn -> {
+            try (PreparedStatement st = conn.prepareStatement("DELETE FROM contact WHERE resume_uuid = ?")) {
+                st.setString(1, r.getUuid());
+                st.execute();
+            }
+            return null;
+        });
     }
 }
 
